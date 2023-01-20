5e38a8a5eb50c3fd1a4b084089cd6fa5e42b80b3
SammyG1996 <sgonzalezjr1914@gmail.com>
2023-01-20 07:04:16 -0500

    finished project

---
+++ routes/auth.js
@@ -0,0 +1,50 @@
+const jwt = require("jsonwebtoken");
+const Router = require("express").Router;
+const router = new Router();
+
+const User = require("../models/user");
+const {SECRET_KEY} = require("../config");
+const ExpressError = require("../expressError");
+
+
+/** POST /login - login: {username, password} => {token}
+ *
+ * Make sure to update their last-login!
+ *
+ **/
+
+router.post('login', async (req, res, next) => {
+  try {
+    const {username, password} = req.body;
+    if(User.authenticate(username, password)){
+      let token =jwt.sign({username}, SECRET_KEY);
+      User.updateLoginTimestamp(username);
+      return res.json({token})
+    } else{
+      throw new ExpressError('Invalid Username/Password', 400);
+    }
+  } catch (error) {
+    return next(error)
+  }
+})
+
+
+/** POST /register - register user: registers, logs in, and returns token.
+ *
+ * {username, password, first_name, last_name, phone} => {token}.
+ *
+ *  Make sure to update their last-login!
+ */
+router.post('/register', (req, res, next) => {
+  try {
+    const {username} = await User.register(req.body);
+    const token = jwt.sign({username}, SECRET_KEY);
+    User.updateLoginTimestamp(username);
+    return res.json({token});
+
+  } catch (error) {
+    return next(error)
+  }
+})
+
+module.exports = router;
